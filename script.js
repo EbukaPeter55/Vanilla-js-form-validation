5e340bbe0591f5f0d960f570af251a8e2a5407c0
Ebuka <peterdbrainy5@gmail.com>
2020-12-13 14:18:00 -0800

    saved all classes and id on a constant variable, created a function to executes on a successful submit

---
+++ script.js
@@ -0,0 +1,12 @@
+const form = document.getElementById('form');
+const password1El = document.getElementById('password1');
+const password2El = document.getElementById('password2');
+const messageContainer = document.querySelector('.message-container');
+const message = document.getElementById('message');
+
+function processFormData(){
+    
+}
+
+//Event listener
+form.addEventListener('submit', processFormData);
